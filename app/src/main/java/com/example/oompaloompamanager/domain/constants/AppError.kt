2cfcdf70a86eb63c936afc502e47848ae05ac6fe
vep1940 <victormanu_96@hotmail.com>
2021-07-18 22:06:03 +0200

    Adding error messages

--- app/src/main/java/com/example/oompaloompamanager/domain/constants/AppError.kt
+++ app/src/main/java/com/example/oompaloompamanager/domain/constants/AppError.kt
@@ -1,11 +1,11 @@
 package com.example.oompaloompamanager.domain.constants
 
 enum class AppError (val code: Int, val message: String) {
-    UNSUCCESSFUL_REQUEST_ERROR(10, ""),
-    EMPTY_BODY_RESPONSE_ERROR(11, ""),
-    NO_INTERNET_ERROR(12, ""),
-    HTTP_ERROR(13, ""),
-    PARSING_ERROR(14, ""),
-    NO_MORE_PAGES_ERROR(15, ""),
-    UNKNOWN_ERROR(100, "")
+    UNSUCCESSFUL_REQUEST_ERROR(10, "Unsuccessful request"),
+    EMPTY_BODY_RESPONSE_ERROR(11, "Empty body response"),
+    NO_INTERNET_ERROR(12, "No internet"),
+    HTTP_ERROR(13, "Http error"),
+    PARSING_ERROR(14, "Parsing error"),
+    NO_MORE_PAGES_ERROR(15, "No more pages to retrieve"),
+    UNKNOWN_ERROR(100, "Unknown error")
 }
--- app/src/main/java/com/example/oompaloompamanager/presentation/BaseFragment.kt
+++ app/src/main/java/com/example/oompaloompamanager/presentation/BaseFragment.kt
@@ -1,7 +1,9 @@
 package com.example.oompaloompamanager.presentation
 
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.oompaloompamanager.BuildConfig
 import com.example.oompaloompamanager.R
 import com.example.oompaloompamanager.domain.constants.AppError
 
@@ -9,18 +11,34 @@ abstract class BaseFragment : Fragment() {
 
     private var toast: Toast? = null
 
-    fun showError(error: AppError){
+    fun showError(error: AppError) {
+        if (BuildConfig.DEBUG) {
+            Log.d(
+                "AppError",
+                getString(R.string.error_message, error.code.toString(), error.message)
+            )
+        }
         toast?.apply {
             this.cancel()
         }
         Toast.makeText(
             context,
-            getString(
-                R.string.error_message,
-                error.code.toString(),
-                error.message
-            ),
+            getMessageError(error),
             Toast.LENGTH_SHORT
         ).show()
+    }
+
+    private fun getMessageError(appError: AppError): String {
+        return when (appError) {
+            AppError.UNSUCCESSFUL_REQUEST_ERROR, AppError.EMPTY_BODY_RESPONSE_ERROR, AppError.NO_INTERNET_ERROR, AppError.HTTP_ERROR -> {
+                getString(R.string.request_failure_error)
+            }
+            AppError.NO_MORE_PAGES_ERROR -> {
+                getString(R.string.no_more_workers_error)
+            }
+            AppError.PARSING_ERROR, AppError.UNKNOWN_ERROR -> {
+                getString(R.string.unknown_error)
+            }
+        }
     }
 }
