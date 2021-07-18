605e37bfc408567324610a38402946cc7bdcce5c
vep1940 <victormanuesp96@gmail.com>
2021-07-18 21:05:21 +0200

    SetUp navController to actionBar

--- app/src/main/java/com/example/oompaloompamanager/presentation/MainActivity.kt
+++ app/src/main/java/com/example/oompaloompamanager/presentation/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.oompaloompamanager.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.oompaloompamanager.R
 import com.example.oompaloompamanager.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding: ActivityMainBinding get() = _binding!!
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         setTheme(R.style.Theme_OompaLoompaManager)
@@ -20,5 +26,14 @@ class MainActivity : AppCompatActivity() {
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
